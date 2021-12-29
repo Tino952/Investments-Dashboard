f41989f30e44bdfcccdcc3a0672025d7dd42b3cb
Tino Leitzmann <cmleitzmann@gmail.com>
2021-12-29 12:00:50 +0100

    deploying app on gh-pages

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ This is an investment dashboard that enables the user navigate dynamically throu
 
 Features of this application:
 
-* Plain vanilla js
-* Build in Webpack
-* Scss
-* Data coming from JSON starter file
-* Sin waves using canvas
+- Plain vanilla js
+- Build in Webpack
+- Scss
+- Data coming from JSON starter file
+- Sin waves using canvas
 
 Please note that this is just a beta version of the app and that the "search by id" feature is not functional.
 
@@ -20,15 +20,15 @@ Click on the output fields containing different investments to navigate downward
 
 ## Under the Hood
 
-* Hover css shows the user what is clickable, and where the navigation ends
-* The user can follow her/his navigation path in the dynamically updating navigation menu
-* HTML canvas effect and color scheme add a futuristic touch to dashboard
+- Hover css shows the user what is clickable, and where the navigation ends
+- The user can follow her/his navigation path in the dynamically updating navigation menu
+- HTML canvas effect and color scheme add a futuristic touch to dashboard
 
 ## Dependencies
 
 To code the sin waves in canvas, I used the [following tutorial](https://www.youtube.com/watch?v=VNmTubIDZOY)
 
-[To the app](https://investment-dashboard-952.herokuapp.com/)
+[To the app](https://tino952.github.io/Investments-Dashboard/)
 
 Cheers!
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,67 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { merge } = require("webpack-merge");
 
-
 const commonConfig = {
-  entry: './client/index.js',
+  entry: "./client/index.js",
   module: {
-      rules: [
-          {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              loader: "babel-loader"
-          },
-          {
-              test: /\.scss/,
-              use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-          }
-      ]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
   },
-    optimization: {
-      minimizer: [
-        new TerserPlugin(),new CssMinimizerPlugin()
-      ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./client/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css"
-        })
-    ]
-  };
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./client/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
 
 const prodConfig = {
-  mode: 'production',
+  mode: "production",
   output: {
     libraryTarget: "var",
     library: "Client",
     // this eliminates the need for the clean webpack plugin
     clean: true,
+    publicPath: "/Investments-Dashboard/",
   },
 };
 
 const devConfig = {
-  mode: 'development',
-  devtool: 'source-map',
-  stats: 'verbose',
+  mode: "development",
+  devtool: "source-map",
+  stats: "verbose",
   optimization: {
     minimize: true,
   },
-
 };
 
-module.exports = (env, arg) =>  {
-  switch(arg.mode) {
+module.exports = (env, arg) => {
+  switch (arg.mode) {
     case "development":
       return merge(commonConfig, devConfig);
     case "production":
       return merge(commonConfig, prodConfig);
     default:
-      throw new Error("no matching configuration found")
+      throw new Error("no matching configuration found");
   }
-}
+};
